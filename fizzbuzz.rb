b6f143fc30c71bc9aa36547ba02067d289b9c5b2
Sheila Wittman <szandora789@gmail.com>
2018-05-08 23:04:20 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,19 +1,14 @@
-def fizzbuzz
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
+   return "FizzBuzz"
+  end
 
-end
+  if int % 5 == 0
+   return "Buzz"
+  end
 
-
-if fizzbuzz_3 = fizzbuzz(3)
-  puts "True"
-
-elsif fizzbuzz_5 = fizzbuzz(5)
-  puts "True"
-
-elsif fizzbuzz_15 = fizzbuzz(3) && fizzbuzz(5)
-  puts "True"
-
-else
-  puts "False"
-
+  if int % 3 == 0
+   return "Fizz"
+  end
 
 end
