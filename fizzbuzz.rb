475a04802d33e81c69438d6e52d6855352996204
Sheila Wittman <szandora789@gmail.com>
2018-05-08 23:02:08 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,5 +4,16 @@ end
 
 
 if fizzbuzz_3 = fizzbuzz(3)
+  puts "True"
+
+elsif fizzbuzz_5 = fizzbuzz(5)
+  puts "True"
+
+elsif fizzbuzz_15 = fizzbuzz(3) && fizzbuzz(5)
+  puts "True"
+
+else
+  puts "False"
+
 
 end
